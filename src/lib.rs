b4162a246f13db7a0aada4d0b0758c748569813c
Breadinator <br3adina7or@gmail.com>
2020-07-15 04:36:14 +0100

    Added Tri, and perimeter methods

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,8 @@ use std::f64::consts::PI;
 
 /// Polygon trait for structs representing 2-dimensional shapes.
 pub trait Polygon {
-	fn area(&self) -> f64;
+	fn area(&self)    -> f64;
+	fn peri(&self) -> f64;
 }
 
 /// Struct that represents a rectangle.
@@ -40,7 +41,20 @@ impl Polygon for Rect {
 	/// let area = rect.area();
 	/// assert_eq!(area, 50.0);
 	/// ```
-	fn area(&self) -> f64 { (&self.width * &self.height) as f64 }
+	fn area(&self) -> f64 {
+		(&self.width * &self.height) as f64
+	}
+
+	/// Gets the perimeter of the Rect from its width and height,
+	/// # Examples
+	/// ```
+	/// let rect = polys::Rect::new(10.0, 5.0);
+	/// let peri = rect.peri();
+	/// assert_eq!(peri, 30f64);
+	/// ```
+	fn peri(&self) -> f64 {
+		2f64 * &self.width + 2f64 * &self.height
+	}
 }
 
 /// Struct that represents a circle.
@@ -58,11 +72,69 @@ impl Circle {
 }
 
 impl Polygon for Circle {
-	///
+	/// Gets the area of the Circle from its radius.
 	/// ```
 	/// let circle = polys::Circle::new(5.0);
 	/// let area = circle.area();
 	/// assert_eq!(area, (std::f64::consts::PI * 25) as f64);
 	/// ```
-	fn area(&self) -> f64 { (PI * &self.radius * &self.radius) as f64 }
+	fn area(&self) -> f64 {
+		(PI * &self.radius * &self.radius) as f64
+	}
+
+	/// Gets the circumferance of the Circle from its radius.
+	/// ```
+	/// let circle = polys::Circle::new(5.0);
+	/// let peri = circle.peri();
+	/// assert_eq(peri, 4f64*PI);
+	/// ```
+	fn peri(&self) -> f64 {
+		(2f64 * PI * &self.radius) as f64
+	}
+}
+
+/// Struct that represents a triangle.
+#[derive(Debug)]
+pub struct Tri {
+	pub side1: f64,
+	pub side2: f64,
+	pub side3: f64,
+}
+
+impl Tri {
+	/// Returns a new Tri from given sides *s1*, *s2*, *s3*.
+	/// # Examples
+	/// ```
+	/// let tri = polys::Tri::new(24.0, 30.0, 18.0);
+	/// ```
+	pub fn new(s1: f64, s2: f64, s3: f64) -> Tri {
+		Tri {side1: s1, side2: s2, side3: s3}
+	}
+}
+
+impl Polygon for Tri {
+	/// Gets the area of the Tri from its sides.
+	/// # Examples
+	/// ```
+	/// let tri = polys::Tri::new(24.0, 30.0, 18.0);
+	/// let area = tri.area();
+	/// assert_eq!(area, 216 as f64);
+	/// ```
+	fn area(&self) -> f64 {
+		let [a, b, c] = [&self.side1, &self.side2, &self.side3];
+		let p: f64 = ((a+b+c)/2.0) as f64;
+		let squared: f64 = p*(p-a)*(p-b)*(p-c);
+		squared.sqrt()
+	}
+
+	/// Gets the perimeter of the Tri from its sides.
+	/// # Examples
+	/// ```
+	/// let tri = polys::Tri::new(24.0, 30.0, 18.0);
+	/// let peri = tri.peri();
+	/// assert_eq!(peri, 72f64);
+	/// ```
+	fn peri(&self) -> f64 {
+		(&self.side1 + &self.side2 + &self.side3) as f64
+	}
 }
